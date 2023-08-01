a4660be8a5dbd9cac4e55d0ce87c1a4c249c5142
gabrielpoto <potogabriel1@gmail.com>
2023-08-01 14:16:13 -0400

    Configure route CRUD

--- app/server.js
+++ app/server.js
@@ -1,19 +1,23 @@
 
 const fastify = require('fastify')({ logger: true });
+const routes =  require('./routes')
+const fastifyPostgres = require('@fastify/postgres');
 
-fastify.register(require('fastify-postgres'), {
+
+fastify.register(fastifyPostgres, {
     connectionString: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_SERVICE}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
 });
 
 
 
-fastify.register(require('./routes'));
+fastify.register(routes);
 
 // run the server
 
 const start = async () => {
     try {
-        await fastify.listen(3000);
+        await fastify.listen(3000, '0.0.0.0');
+        console.log(`Serveur en écoute sur le port ${fastify.server.address().port}`);
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
